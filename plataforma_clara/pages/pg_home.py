3ae59a30a7540e3745f60cd1ce59e748b8f5a1c4
Moreira-89 <contatoparalucasmoreira@gmail.com>
2026-03-28 22:28:48 -0300

    feat: adicionei a biblioteca do google. Coloquei as rotas principais na pagina home.

--- plataforma_clara/pages/pg_home.py
+++ plataforma_clara/pages/pg_home.py
@@ -5,6 +5,9 @@ def home_page():
         rx.vstack(
             rx.heading("Bem-vindo à Plataforma Clara!", size="1"),
             rx.text("Acesse o menu para navegar pelas funcionalidades."),
+            rx.button("Login", on_click=rx.redirect("/login-usuario")),
+            rx.button("Cadastro de Gestora", on_click=rx.redirect("/cadastro-gestora")),
+            rx.button("Cadastro de Investidor", on_click=rx.redirect("/cadastro-investidor")),
             spacing="4",
         )
     )
