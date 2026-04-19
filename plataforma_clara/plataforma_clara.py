8539949498a21e9272b48aba1d8062454236c9c4
Moreira-89 <contatoparalucasmoreira@gmail.com>
2026-04-19 20:48:21 -0300

    refactor: remove unused dashboard page imports from the main application file

--- plataforma_clara/plataforma_clara.py
+++ plataforma_clara/plataforma_clara.py
@@ -2,11 +2,8 @@ from plataforma_clara.pages.pg_cadastro_usuario import formulario_cadastro_gesto
 from plataforma_clara.pages.pg_ingestao_dados import ingestao_dados
 from plataforma_clara.pages.pg_login import formulario_login
 from plataforma_clara.pages.pg_home import home_page
-from plataforma_clara.pages.pg_dashboard_gestora import dashboard_gestora
-from plataforma_clara.pages.pg_dashboard_investidor import dashboard_investidor
 
 import reflex as rx
-
 
 app = rx.App()
 app.add_page(home_page, route="/", title="Plataforma Clara - Home")
